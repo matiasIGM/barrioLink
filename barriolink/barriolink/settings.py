a11d3627cca9790114ed381aff8d9e5c5fe97fa6
Matias Garces Martinez <54512479+matiasIGM@users.noreply.github.com>
2023-11-19 06:11:29 -0300

    clean settings

--- barriolink/barriolink/settings.py
+++ barriolink/barriolink/settings.py
@@ -10,7 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
-from decouple import config, Csv
+from decouple import config
 from pathlib import Path
 import os
 from typing import (List, Dict, Tuple, Any)
