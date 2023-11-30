115033b01f179c7359b3088b249b4f28c88f9856
Matias Garces Martinez <54512479+matiasIGM@users.noreply.github.com>
2023-11-29 21:00:35 -0300

    psw reset template url

--- barriolink/main/urls.py
+++ barriolink/main/urls.py
@@ -47,13 +47,13 @@ urlpatterns = [
      path('edit-hoa-config/<int:hoa_id>/', views.editHoaConfig, name='edit_hoa_data'),
      path('update-hoa-config/<int:hoa_id>/', views.updateHoaConfig, name='update_hoa_data'),
 
-     path('password_reset/', auth_view.PasswordResetView.as_view(template_name="users/password_reset.html"),
+     path('password_reset/', auth_view.PasswordResetView.as_view(template_name="account/users/password_reset.html"),
            name='password_reset'),
-     path('password_reset_done/', auth_view.PasswordResetDoneView.as_view(template_name="users/password_reset_done.html"),
+     path('password_reset_done/', auth_view.PasswordResetDoneView.as_view(template_name="account/users/password_reset_done.html"),
             name='password_reset_done'),
-     path('password_reset_confirm/<uidb64>/<token>/', auth_view.PasswordResetConfirmView.as_view(template_name="users/password_reset_confirm.html"),
+     path('password_reset_confirm/<uidb64>/<token>/', auth_view.PasswordResetConfirmView.as_view(template_name="account/users/password_reset_confirm.html"),
            name='password_reset_confirm'),
-     path('password_reset_complete/', auth_view.PasswordResetCompleteView.as_view(template_name="users/password_reset_complete.html"),
+     path('password_reset_complete/', auth_view.PasswordResetCompleteView.as_view(template_name="account/users/password_reset_complete.html"),
            name='password_reset_complete'),
       
       
